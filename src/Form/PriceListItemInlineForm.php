ffbd7c9b4599cdb7d1c7784dfaf0af5e586a07d1
LiuXin <LiuXin@2936003.no-reply.drupal.org>
2018-07-17 17:28:56 +0800

    Issue #2984736 by caseylau, alan_blake: PriceListInlineEntityform's ajax is in chaos

--- src/Form/PriceListItemInlineForm.php
+++ src/Form/PriceListItemInlineForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\commerce_pricelist\Form;
 
+use Drupal\Core\Entity\Element\EntityAutocomplete;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
@@ -99,62 +100,100 @@ class PriceListItemInlineForm extends EntityInlineForm {
    */
   public function entityForm(array $entity_form, FormStateInterface $form_state) {
     $entity_form = parent::entityForm($entity_form, $form_state);
-    $entity_form['purchased_entity']['widget'][0]['target_id']['#ajax'] = [
-      'callback' => [get_class($this), 'purchasedRefresh'],
-      'event' => 'autocompleteclose',
-      'wrapper' => 'purchased_entity_refresh',
-    ];
+
+    // This is for widget entity_auto_complete.
+    if (isset($entity_form['purchased_entity']['widget'][0])) {
+      $entity_form['purchased_entity']['widget'][0]['target_id']['#ajax'] = [
+        'callback' => [get_class($this), 'purchasedRefresh'],
+        'event' => 'autocompleteclose',
+        'wrapper' => $entity_form['#ief_row_delta'] . 'purchased_entity_refresh',
+      ];
+    }
+    // This is for widget select similarity.
+    else {
+      $entity_form['purchased_entity']['widget']['#ajax'] = [
+        'callback' => [get_class($this), 'purchasedRefresh'],
+        'wrapper' => $entity_form['#ief_row_delta'] . 'purchased_entity_refresh',
+      ];
+    }
+
     $entity_form = $this->priceForm($entity_form, $form_state);
-    $entity_form['price']['#attributes']['id'] = 'purchased_entity_refresh';
+    $entity_form['price']['#attributes']['id'] = $entity_form['#ief_row_delta'] . 'purchased_entity_refresh';
     return $entity_form;
   }
 
   /**
+   * Handle the disable property of price.
+   *
    * @param array $entity_form
    *   The entity form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   The form state of the parent form.
+   *
    * @return array
    *   The entity form.
+   *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    */
   public function priceForm(array $entity_form, FormStateInterface $form_state) {
     /** @var \Drupal\commerce_pricelist\Entity\PriceListItem $entity */
     $entity = $entity_form['#entity'];
-    $entity_form['price']['#disabled'] = FALSE;
-    $target_id = NULL;
+    $entity_form['price']['widget']['#disabled'] = FALSE;
     if ($entity->hasPurchasedEntity()) {
-      $target_id = $entity->getPurchasedEntityId();
+      $purchasedEntity = $entity->getPurchasedEntity();
     }
+    $userInput = $form_state->getUserInput();
+    // When the operation is 'add'.
+    if ($entity_form['#op'] == 'add') {
+      if (isset($userInput['field_price_list_item']['form']['inline_entity_form']['purchased_entity'])) {
+        $purchasedEntityId = $userInput['field_price_list_item']['form']['inline_entity_form']['purchased_entity'];
+      }
+      if (isset($purchasedEntityId[0]['target_id'])) {
+        $purchasedEntityId = EntityAutocomplete::extractEntityIdFromAutocompleteInput($purchasedEntityId[0]['target_id']);
+      }
+    }
+    // When the operation is 'edit'.
     else {
-      $values = $form_state->getValues();
-      $inline_entity_form = $values['field_price_list_item']['form']['inline_entity_form'];
-      if ($inline_entity_form) {
-        $target_id = $inline_entity_form['purchased_entity'][0]['target_id'];
+      if (isset($userInput['field_price_list_item']['form']['inline_entity_form']['entities'])) {
+        $entities = $userInput['field_price_list_item']['form']['inline_entity_form']['entities'];
+      }
+      if (isset($entities[$entity_form['#ief_row_delta']]['form']['purchased_entity'])) {
+        $purchasedEntityId = $entities[$entity_form['#ief_row_delta']]['form']['purchased_entity'];
+        // If the widget type is entity_auto_complete, we can just get entity id
+        // by using regular, because the entity_id can't be get directly in
+        // inline_entity_form.
+        if (isset($purchasedEntityId[0]['target_id'])) {
+          $purchasedEntityId = EntityAutocomplete::extractEntityIdFromAutocompleteInput($purchasedEntityId[0]['target_id']);
+        }
       }
     }
-    if ($target_id) {
-      $purchased_entity = $entity->getPurchasedEntity();
-      $price = $purchased_entity->getPrice();
+
+    if (isset($purchasedEntityId)) {
+      $purchasedEntityTargetType = $entity->getFieldDefinition('purchased_entity')->getSetting('target_type');
+      $purchasedEntity = $this->entityTypeManager->getStorage($purchasedEntityTargetType)->load($purchasedEntityId);
+    }
+
+    if (isset($purchasedEntity)) {
+      $price = $purchasedEntity->getPrice();
       if (!$price) {
-        $entity_form['price']['#disabled'] = TRUE;
-      }
-      else {
-        $entity_form['price']['#default_value'] = $price->toArray();
+        $entity_form['price']['widget']['#disabled'] = TRUE;
       }
     }
     return $entity_form;
   }
 
   /**
+   * Auto get the purchased entity's price.
+   *
    * @param array $form
    *   The form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   The form state of the parent form.
+   *
    * @return array
    *   The price element.
    */
-  public function purchasedRefresh(array $form, FormStateInterface $form_state) {
+  public static function purchasedRefresh(array $form, FormStateInterface $form_state) {
     $element = [];
     $triggering_element = $form_state->getTriggeringElement();
 
@@ -164,10 +203,41 @@ class PriceListItemInlineForm extends EntityInlineForm {
       $element = NestedArray::getValue($form, $array_parents);
       array_pop($array_parents);
     }
-    if ($number = $element['price']['#default_value']['number']) {
-      $element['price']['widget'][0]['number']['#value'] = sprintf("%.2f", $number);
+
+    $elementPrice = $element['price'];
+
+    $targetType = $element['#entity']->getFieldDefinition('purchased_entity')->getSetting('target_type');
+    $fieldPriceListItem = $form_state->getValue('field_price_list_item');
+
+    // When the operation is 'add'.
+    if ($element['#op'] == 'add') {
+      if (isset($fieldPriceListItem['form']['inline_entity_form']['purchased_entity'][0]['target_id'])) {
+        $purchasedEntityId = $fieldPriceListItem['form']['inline_entity_form']['purchased_entity'][0]['target_id'];
+      }
     }
-    return $element['price'];
+    // When the operation is 'edit'.
+    else {
+      $entities = $fieldPriceListItem['form']['inline_entity_form']['entities'];
+      if (isset($entities[$element['#ief_row_delta']]['form']['purchased_entity'][0]['target_id'])) {
+        $purchasedEntityId = $entities[$element['#ief_row_delta']]['form']['purchased_entity'][0]['target_id'];
+      }
+    }
+
+    if (isset($purchasedEntityId)) {
+      // Because there's no object($this) in the ajax callback when we use
+      // inline_entity_form, so we can't use $this->entityTypeManager directly.
+      /** @var \Drupal\commerce\PurchasableEntityInterface $purchasedEntity */
+      $purchasedEntity = \Drupal::entityTypeManager()->getStorage($targetType)->load($purchasedEntityId);
+      $price = $purchasedEntity->getPrice();
+      if (!is_null($price)) {
+        $elementPrice['widget'][0]['number']['#value'] = sprintf("%.2f", $price->getNumber());
+      }
+      else {
+        $elementPrice['widget'][0]['number']['#value'] = NULL;
+        $elementPrice['widget'][0]['number']['#placeholder'] = NULL;
+      }
+    }
+    return $elementPrice;
   }
 
   /**
@@ -222,7 +292,7 @@ class PriceListItemInlineForm extends EntityInlineForm {
    * {@inheritdoc}
    */
   public function getEntityLabel(EntityInterface $entity) {
-    return is_null($entity->label())?$entity->getPurchasedEntity()->label():$entity->label();
+    return is_null($entity->label()) ? (empty($entity->getPurchasedEntity()) ? NULL : $entity->getPurchasedEntity()->label()) : $entity->label();
   }
 
 }
