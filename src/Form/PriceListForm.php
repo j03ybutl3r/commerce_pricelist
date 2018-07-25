9824c66d64974808cb51ffbc8302287c1f911c14
Bojan Zivanovic <bojanz@gmail.com>
2018-07-25 13:55:34 +0200

    Issue #2987900 part I: Improve the price list form (Re-add the two-column layout)

--- src/Form/PriceListForm.php
+++ src/Form/PriceListForm.php
@@ -28,10 +28,77 @@ class PriceListForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+    $form['#tree'] = TRUE;
+    $form['#theme'] = ['commerce_pricelist_form'];
+    $form['#attached']['library'][] = 'commerce_pricelist/form';
+
+    $form['advanced'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['entity-meta']],
+      '#weight' => 99,
+    ];
+    $form['option_details'] = [
+      '#type' => 'container',
+      '#title' => $this->t('Options'),
+      '#group' => 'advanced',
+      '#attributes' => ['class' => ['entity-meta__header']],
+      '#weight' => -100,
+    ];
+    $form['date_details'] = [
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#title' => $this->t('Dates'),
+      '#group' => 'advanced',
+    ];
+
+    $field_details_mapping = [
+      'status' => 'option_details',
+      'weight' => 'option_details',
+      'start_date' => 'date_details',
+      'end_date' => 'date_details',
+    ];
+    foreach ($field_details_mapping as $field => $group) {
+      if (isset($form[$field])) {
+        $form[$field]['#group'] = $group;
+      }
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function actions(array $form, FormStateInterface $form_state) {
+    $actions = parent::actions($form, $form_state);
+
+    if ($this->entity->isNew()) {
+      $actions['submit_continue'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Save and add prices'),
+        '#continue' => TRUE,
+        '#submit' => ['::submitForm', '::save'],
+      ];
+    }
+
+    return $actions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state) {
     $this->entity->save();
     drupal_set_message($this->t('Saved the %label price list.', ['%label' => $this->entity->label()]));
-    $form_state->setRedirect('entity.commerce_pricelist.collection');
+
+    if (!empty($form_state->getTriggeringElement()['#continue'])) {
+      $form_state->setRedirect('entity.commerce_pricelist_item.collection', ['commerce_pricelist' => $this->entity->id()]);
+    }
+    else {
+      $form_state->setRedirect('entity.commerce_pricelist.collection');
+    }
   }
 
 }
